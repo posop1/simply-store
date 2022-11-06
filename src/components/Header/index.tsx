0ff629ee0b6ce1f707cb0fa153814142f3629225
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-06 15:24:19 +0500

    add react icons and header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,24 @@
 import { FC } from 'react'
+import { BsBasket } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 import s from './Header.module.scss'
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
-  return <div className={s.Header}>asd</div>
+  return (
+    <div className={s.Header}>
+      <Link to="/">
+        <h1 className={s.logo}>Store</h1>
+      </Link>
+      <Link to="/bin">
+        <BsBasket
+          size={25}
+          className={s.bin}
+        />
+      </Link>
+    </div>
+  )
 }
 
 export default Header
