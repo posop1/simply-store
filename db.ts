dd110b0172ff73a009da303e43c4747ca8cbc1b6
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-06 15:22:58 +0500

    add items and types for items

---
+++ db.ts
@@ -0,0 +1,58 @@
+import { IProduct } from './src/types'
+
+export const items: IProduct[] = [
+  {
+    id: 1,
+    name: 'test',
+    price: 100,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 2,
+    name: 'test',
+    price: 200,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 3,
+    name: 'test',
+    price: 300,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 4,
+    name: 'test',
+    price: 1100,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 5,
+    name: 'test',
+    price: 100,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 6,
+    name: 'test',
+    price: 100,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 7,
+    name: 'test',
+    price: 100,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 8,
+    name: 'test',
+    price: 100,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  },
+  {
+    id: 9,
+    name: 'test',
+    price: 100,
+    img: 'https://klike.net/uploads/posts/2018-12/1544425270_1.jpg'
+  }
+]
