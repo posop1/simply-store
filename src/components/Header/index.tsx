b9a05cd3ba77a625eff03eb493c0cf20b515f1dc
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-06 14:32:05 +0500

    add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import s from './Header.module.scss'
+
+interface HeaderProps {}
+
+const Header: FC<HeaderProps> = () => {
+  return <div className={s.Header}>asd</div>
+}
+
+export default Header
