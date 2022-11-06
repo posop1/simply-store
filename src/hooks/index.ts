498ad9fadfffc6dbccc2d79edfce83c3e55d9ec4
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-06 20:38:54 +0500

    fix useSelector custom hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,4 +2,4 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { AppDispatch, RootState } from '../redux/store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
