ef04bdd087a28f3ad2a5ccae8c42126793dd92fe
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-06 14:31:55 +0500

    add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import BinPage from './pages/BinPage'
+import HomePage from './pages/HomePage'
 import './styles/app.scss'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/bin"
+          element={<BinPage />}
+        />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.scss'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
