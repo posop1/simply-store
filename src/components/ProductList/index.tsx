bf98048aa391706bc589c004bc28e13ffa1684e8
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-07 16:21:20 +0500

    feat(product list): add full functional

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,10 +1,24 @@
 import { FC } from 'react'
+import { useAppSelector } from '../../hooks'
+import ProductItem from './ProductItem'
 import s from './ProductList.module.scss'
 
-interface ProductListProps {}
+const ProductList: FC = () => {
+  const products = useAppSelector((state) => state.products.products)
 
-const ProductList: FC<ProductListProps> = () => {
-  return <div className={s.ProductList}>ProductList</div>
+  return (
+    <div className={s.ProductList}>
+      {products.map((product) => (
+        <ProductItem
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          price={product.price}
+          img={product.img}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default ProductList
