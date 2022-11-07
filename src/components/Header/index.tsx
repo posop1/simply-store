1a2bf7dd6b7fd6be3b04e4a9baa47623937a6837
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-07 16:20:23 +0500

    feat(header): bin functional

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,34 @@
 import { FC } from 'react'
 import { BsBasket } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
+import { useAppSelector } from '../../hooks'
 import s from './Header.module.scss'
 
-interface HeaderProps {}
+const Header: FC = () => {
+  const binProducts = useAppSelector((state) => state.products.bin).length
 
-const Header: FC<HeaderProps> = () => {
   return (
     <div className={s.Header}>
       <Link to="/">
         <h1 className={s.logo}>Store</h1>
       </Link>
-      <Link to="/bin">
-        <BsBasket
-          size={25}
+      {binProducts ? (
+        <Link
+          to="/bin"
           className={s.bin}
-        />
-      </Link>
+        >
+          <BsBasket size={25} />
+          <hr />
+          <p>{binProducts}</p>
+        </Link>
+      ) : (
+        <Link
+          to="/bin"
+          className={s.bin}
+        >
+          <BsBasket size={25} />
+        </Link>
+      )}
     </div>
   )
 }
